ef5d06d47a3ab2b5b513c5629fb7fae45a16d010
asadaravani <abegano1@stu.vistula.edu.pl>
2024-04-21 17:01:16 +0600

    update

--- src/main/java/kg/group8/neotour/config/CorsConfig.java
+++ src/main/java/kg/group8/neotour/config/CorsConfig.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+        registry.addMapping("/*")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
