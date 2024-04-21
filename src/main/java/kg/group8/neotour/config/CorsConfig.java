c5c0e0bd2796f3a7ef6d98de5efa6682a8532197
asadaravani <abegano1@stu.vistula.edu.pl>
2024-04-21 18:15:48 +0600

    update config

--- src/main/java/kg/group8/neotour/config/CorsConfig.java
+++ src/main/java/kg/group8/neotour/config/CorsConfig.java
@@ -3,14 +3,15 @@ package kg.group8.neotour.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
-                .allowedOriginPatterns("https://neotour-production-8755.up.railway.app/*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOriginPatterns("/*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                //.allowedHeaders("*");
+                //.allowCredentials(true);
     }
 }
